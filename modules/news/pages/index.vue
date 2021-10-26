524836dbc5e41e70d8549c2edbb78bbf0f2cde42
ElijahCoding <cuiyeqing960904@gmail.com>
2021-10-26 03:36:21 +0300

    news index page

---
+++ modules/news/pages/index.vue
@@ -0,0 +1,152 @@
+<template>
+    <div class="d-flex flex-column flex-grow-1">
+        <div class="d-flex align-center py-3 pb-0">
+            <div>
+                <div class="display-1">Новости</div>
+                <v-breadcrumbs :items="breadcrumbs" class="pa-0 py-2"></v-breadcrumbs>
+            </div>
+        </div>
+
+        <div class="mb-2">
+            <v-btn :to="{ name: 'news.create' }"> Добавить новость </v-btn>
+        </div>
+
+        <advanced-search-form :filters="filters" :value="searchForm" @search="search" />
+
+        <v-card>
+            <v-data-table
+                v-model="selectedItems"
+                item-key="id"
+                :headers="headers"
+                :items="news"
+                :loading="isLoading"
+                :server-items-length="total"
+                loading-text="Идет загрузка..."
+                :options.sync="tableOptions"
+                :footer-props="tableFooterProps"
+                show-select
+                @update:items-per-page="updateOptions('itemsPerPage', $event)"
+                @update:page="updateOptions('page', $event)"
+                @update:sort-by="updateOptions('sortBy', $event)"
+                @update:sort-desc="updateOptions('sortDesc', $event)"
+            >
+                <template #item.id="{ item }">
+                    <div class="font-weight-bold text-no-wrap"># {{ item.id }}</div>
+                </template>
+
+                <template #item.published_at="{ item }">
+                    <div>{{ item.asDate('published_at').fromNow() }}</div>
+                </template>
+
+                <template #item.is_in_home="{ item }">
+                    <div>{{ item.is_in_home ? 'Да' : 'Нет' }}</div>
+                </template>
+
+                <template #item.action="{ item }">
+                    <div class="actions text-no-wrap">
+                        <v-btn icon width="22" height="22" :to="{ name: 'news.update', params: { id: item.id } }">
+                            <pencil-alt-icon class="h-6 w-6" />
+                        </v-btn>
+                        <v-btn icon width="22" height="22" class="mx-1" @click.prevent="deleteNews(item)">
+                            <trash-icon class="h-6 w-6" />
+                        </v-btn>
+                    </div>
+                </template>
+            </v-data-table>
+        </v-card>
+    </div>
+</template>
+
+<script>
+import DatatableMixin from '@/mixins/datatable';
+import AdvancedSearchForm from '@/components/search/AdvancedSearchForm';
+import { enumToSelectArray, StatusDescription } from '@/enums';
+import News from '../models/News';
+
+export default {
+    components: {
+        AdvancedSearchForm,
+    },
+    mixins: [DatatableMixin],
+    data() {
+        return {
+            news: [],
+            searchForm: {
+                name: null,
+                is_in_home: null,
+                status: null,
+            },
+            headers: [
+                { text: 'ID', align: 'left', value: 'id' },
+                { text: 'Название', align: 'left', value: 'name' },
+                { text: 'Ссылка', align: 'left', value: 'slug' },
+                { text: 'Дата новости', align: 'left', value: 'published_at' },
+                { text: 'Статус', value: 'status.description' },
+                { text: 'На главной', value: 'is_in_home' },
+                { text: '', sortable: false, align: 'right', value: 'action' },
+            ],
+            breadcrumbs: [{ text: 'Главная', href: '/' }, { text: 'Список новостей' }],
+            filters: [
+                {
+                    label: 'Название',
+                    name: 'name',
+                    component: () => import('@/components/search/fields/TextSearchField'),
+                },
+                {
+                    label: 'ID',
+                    name: 'id',
+                    component: () => import('@/components/search/fields/ComboBoxSearchField'),
+                },
+                {
+                    label: 'Ссылка',
+                    name: 'slug',
+                    component: () => import('@/components/search/fields/TextSearchField'),
+                },
+                {
+                    label: 'Статус',
+                    name: 'status',
+                    component: () => import('@/components/search/fields/SelectSearchField'),
+                    items: enumToSelectArray(StatusDescription),
+                },
+                {
+                    label: 'Отображается на главной',
+                    name: 'is_in_home',
+                    component: () => import('@/components/search/fields/BooleanSelectSearchField'),
+                },
+            ],
+        };
+    },
+    async fetch() {
+        this.showLoading();
+
+        const response = await News.select({
+            news: ['id', 'name', 'slug', 'status', 'published_at', 'is_in_home'],
+        })
+            .params(this.queryParams)
+            .get();
+
+        this.news = News.hydrate(response.data);
+
+        this.setTotal(response.meta.total);
+        this.hideLoading();
+    },
+    head: {
+        title: 'Новости',
+    },
+    methods: {
+        async deleteNews(news) {
+            if (!(await this.$confirm(`Вы действительно хотите удалить новость ${news.name}?`))) {
+                return;
+            }
+            try {
+                await news.delete();
+
+                this.$snackbar(`Новость ${news.name} успешно удалена`);
+                this.news = this.news.filter((item) => item.id !== news.id);
+            } catch (e) {
+                this.$snackbar(e.message);
+            }
+        },
+    },
+};
+</script>
