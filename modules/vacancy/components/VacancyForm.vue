39bbfa52b34e2f34a1a538f45efc24a154e83afc
ElijahCoding <cuiyeqing960904@gmail.com>
2021-11-02 19:00:44 +0300

    vacancies built

--- modules/vacancy/components/VacancyForm.vue
+++ modules/vacancy/components/VacancyForm.vue
@@ -1,45 +1,50 @@
 <template>
-    <v-form @submit.prevent="$emit('send', form)">
-        <v-text-field
-            v-model="form.name"
-            label="Название"
-            :error-messages="form.errors.get('name')"
-            :error="form.errors.has('name')"
-        />
+    <v-expansion-panel>
+        <v-expansion-panel-header class="title">Основная информация</v-expansion-panel-header>
+        <v-expansion-panel-content>
+            <v-form @submit.prevent="$emit('send', form)">
+                <v-text-field
+                    v-model="form.name"
+                    label="Название"
+                    :error-messages="form.errors.get('name')"
+                    :error="form.errors.has('name')"
+                />
 
-        <v-text-field
-            v-model="form.slug"
-            label="Ссылка"
-            :error-messages="form.errors.get('slug')"
-            :error="form.errors.has('slug')"
-        />
+                <v-text-field
+                    v-model="form.slug"
+                    label="Ссылка"
+                    :error-messages="form.errors.get('slug')"
+                    :error="form.errors.has('slug')"
+                />
 
-        <v-textarea
-            v-model="form.short_description"
-            label="Краткое описание"
-            :error-messages="form.errors.get('short_description')"
-            :error="form.errors.has('short_description')"
-        />
-        <v-input
-            label="Подробное описание"
-            :error-messages="form.errors.get('full_description')"
-            :error="form.errors.has('full_description')"
-        >
-            <content-editor v-model="form.full_description" />
-        </v-input>
+                <v-select
+                    v-model="form.status"
+                    label="Статус"
+                    :items="statusLabels"
+                    :error-messages="form.errors.get('status')"
+                    :error="form.errors.has('status')"
+                />
 
-        <v-select
-            v-model="form.status"
-            label="Статус"
-            :items="statusLabels"
-            :error-messages="form.errors.get('status')"
-            :error="form.errors.has('status')"
-        />
+                <v-textarea
+                    v-model="form.short_description"
+                    label="Краткое описание"
+                    :error-messages="form.errors.get('short_description')"
+                    :error="form.errors.has('short_description')"
+                />
+                <v-input
+                    label="Подробное описание"
+                    :error-messages="form.errors.get('full_description')"
+                    :error="form.errors.has('full_description')"
+                >
+                    <content-editor v-model="form.full_description" />
+                </v-input>
 
-        <slot name="buttons">
-            <v-btn type="submit">Сохранить</v-btn>
-        </slot>
-    </v-form>
+                <slot name="buttons">
+                    <v-btn type="submit">Сохранить</v-btn>
+                </slot>
+            </v-form>
+        </v-expansion-panel-content>
+    </v-expansion-panel>
 </template>
 
 <script>
@@ -65,6 +70,7 @@ export default {
             slug: null,
             short_description: null,
             full_description: null,
+            status: 1,
         },
         form: null,
         statusLabels,
