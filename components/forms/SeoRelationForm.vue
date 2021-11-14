3096667b0fe9907e7e22324ac235b586ba3c8f94
ElijahCoding <cuiyeqing960904@gmail.com>
2021-11-14 19:45:49 +0300

    seo meta tags built

--- components/forms/SeoRelationForm.vue
+++ components/forms/SeoRelationForm.vue
@@ -28,6 +28,35 @@
                 :error-messages="form.errors.get('description')"
                 :error="form.errors.has('description')"
             />
+            <v-card outlined>
+                <v-card-title class="d-flex justify-space-between align-center">
+                    <h4>Мета теги</h4>
+                    <v-btn type="button" small dark color="orange" @click.prevent="addTag">Добавить</v-btn>
+                </v-card-title>
+                <v-card-text class="pt-0 pb-0">
+                    <div v-for="(tag, index) in form.meta_tags" :key="index" class="d-flex mb-2 align-center">
+                        <v-col cols="5">
+                            <v-text-field
+                                v-model="tag.name"
+                                label="Название"
+                                :error-messages="form.errors.get(`meta_tags.${index}.name`)"
+                                :error="form.errors.has(`meta_tags.${index}.name`)"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="5">
+                            <v-text-field
+                                v-model="tag.content"
+                                label="Контент"
+                                :error-messages="form.errors.get(`meta_tags.${index}.content`)"
+                                :error="form.errors.has(`meta_tags.${index}.content`)"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="2">
+                            <v-btn x-small dark color="red" @click="deleteTag(index)">удалить</v-btn>
+                        </v-col>
+                    </div>
+                </v-card-text>
+            </v-card>
         </template>
         <v-row class="expansion-panel-actions mt-5">
             <v-col>
@@ -38,7 +67,7 @@
 </template>
 
 <script>
-import {Form} from 'form-backend-validation';
+import { Form } from 'form-backend-validation';
 
 export default {
     props: {
@@ -53,19 +82,33 @@ export default {
             title: null,
             h1: null,
             description: null,
+            meta_tags: [],
         },
         form: null,
     }),
-    created() {
-        this.form = Form.create(this.formDefaults)
-            .withOptions({http: this.$axios, resetOnSuccess: false})
-            .populate(this.seo || {})
-    },
     watch: {
         seo(value) {
             this.form.populate(value);
         },
-    }
-}
+    },
+    created() {
+        this.form = Form.create(this.formDefaults)
+            .withOptions({ http: this.$axios, resetOnSuccess: false })
+            .populate(this.seo || {});
+    },
+    methods: {
+        addTag() {
+            if (!this.form.meta_tags) {
+                this.form.meta_tags = [];
+            }
+            this.form.meta_tags.push({
+                name: null,
+                content: null,
+            });
+        },
+        deleteTag(index) {
+            this.form.meta_tags.splice(index, 1);
+        },
+    },
+};
 </script>
-
