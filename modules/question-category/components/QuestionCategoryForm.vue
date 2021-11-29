ad21642cdc9419334f2d1d0ad669aa79b5a882e1
ElijahCoding <cuiyeqing960904@gmail.com>
2021-11-29 16:22:06 +0300

    btn fixed

--- modules/question-category/components/QuestionCategoryForm.vue
+++ modules/question-category/components/QuestionCategoryForm.vue
@@ -22,9 +22,11 @@
             :error="form.errors.has('status')"
         />
 
-        <slot name="buttons">
-            <v-btn type="submit">Сохранить</v-btn>
-        </slot>
+        <v-row class="expansion-panel-actions mt-5">
+            <v-col>
+                <v-btn type="submit" color="green" class="white--text text-uppercase">Сохранить</v-btn>
+            </v-col>
+        </v-row>
     </v-form>
 </template>
 
