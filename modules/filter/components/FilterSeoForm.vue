b7b0ab227e9046b09e45bade2114a53671e3acbf
ElijahCoding <cuiyeqing960904@gmail.com>
2021-12-10 05:57:36 +0300

    building seo

--- modules/filter/components/FilterSeoForm.vue
+++ modules/filter/components/FilterSeoForm.vue
@@ -1,1 +1,37 @@
-<template></template>
+<template>
+    <v-form @submit.prevent="send"> </v-form>
+</template>
+
+<script>
+import { Form } from 'form-backend-validation';
+
+export default {
+    props: {
+        filter: {
+            type: Object | null,
+            default: () => ({}),
+        },
+    },
+    data: () => ({
+        formDefaults: {
+            options: {
+                name: null,
+                path: null,
+                value: null,
+            },
+        },
+        form: null,
+    }),
+    watch: {
+        filter(value) {
+            this.form.populate(value);
+        },
+    },
+    created() {
+        this.form = Form.create(this.formDefaults)
+            .withOptions({ http: this.$axios })
+            .populate(this.filter || {});
+    },
+    methods: {},
+};
+</script>
