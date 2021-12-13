49107b94dad7ceedbdce940bfcd29bca79098789
ElijahCoding <cuiyeqing960904@gmail.com>
2021-12-13 15:37:54 +0300

    cabinet store

---
+++ store/cabinet.js
@@ -0,0 +1,35 @@
+import Cabinet from '~/modules/cabinet/models/Cabinet';
+import Product from '~/modules/product/models/Product';
+
+export default {
+    state: () => ({
+        cabinet: null,
+        cabinetSeo: null,
+    }),
+
+    mutations: {
+        SET_CABINET(state, data) {
+            state.cabinet = data;
+        },
+        SET_CABINET_SEO(state, data) {
+            state.cabinetSeo = data;
+        },
+    },
+
+    getters: {
+        cabinet(state) {
+            return state.cabinet;
+        },
+        cabinetSeo(state) {
+            return state.cabinetSeo;
+        },
+    },
+
+    actions: {
+        async getCabinet({ commit }, id) {
+            const cabinet = await Cabinet.query().with('seo', 'categories').$find(id);
+            commit('SET_CABINET', cabinet);
+            commit('SET_CABINET_SEO', cabinet.seo || {});
+        },
+    },
+};
