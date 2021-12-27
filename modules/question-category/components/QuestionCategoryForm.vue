795415a15ff892f546196a6e13ea8efeea9b9dfd
ElijahCoding <cuiyeqing960904@gmail.com>
2021-12-28 01:34:05 +0300

    built

--- modules/question-category/components/QuestionCategoryForm.vue
+++ modules/question-category/components/QuestionCategoryForm.vue
@@ -22,6 +22,15 @@
             :error="form.errors.has('status')"
         />
 
+        <v-text-field
+            v-if="isUpdating"
+            v-model="form.position"
+            type="number"
+            label="Позиция"
+            :error-messages="form.errors.get('position')"
+            :error="form.errors.has('position')"
+        />
+
         <v-row class="expansion-panel-actions mt-5">
             <v-col>
                 <v-btn type="submit" color="green" class="white--text text-uppercase">Сохранить</v-btn>
@@ -50,6 +59,7 @@ export default {
             name: null,
             slug: null,
             status: 1,
+            position: null,
         },
         form: null,
         statusLabels,
--- modules/question/components/QuestionForm.vue
+++ modules/question/components/QuestionForm.vue
@@ -37,6 +37,16 @@
             :error-messages="form.errors.get('status')"
             :error="form.errors.has('status')"
         />
+
+        <v-text-field
+            v-if="isUpdating"
+            v-model="form.position"
+            type="number"
+            label="Позиция"
+            :error-messages="form.errors.get('position')"
+            :error="form.errors.has('position')"
+        />
+
         <v-row class="expansion-panel-actions mt-5">
             <v-col>
                 <v-btn type="submit" color="green" class="white--text text-uppercase">Сохранить</v-btn>
@@ -71,6 +81,7 @@ export default {
             answer: null,
             status: 1,
             question_category_id: null,
+            position: null,
         },
         questionCategories: [],
         form: null,
