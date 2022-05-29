fa9a3e907c97707f9cf426a53bcf3ecae1fcba83
ElijahCoding <cuiyeqing960904@gmail.com>
2022-05-29 21:57:40 +0300

    routes built

---
+++ modules/contact/router/index.js
@@ -0,0 +1,17 @@
+export const contactRoutes = (resolve) => [
+    {
+        path: '/contacts',
+        name: 'contacts.index',
+        component: resolve(__dirname, '../pages/index.vue'),
+    },
+    {
+        path: '/contacts/create',
+        name: 'contacts.create',
+        component: resolve(__dirname, '../pages/create.vue'),
+    },
+    {
+        path: '/contacts/:id/update',
+        name: 'contacts.update',
+        component: resolve(__dirname, '../pages/update.vue'),
+    },
+];
