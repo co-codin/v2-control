62092146bb6084e81cbd5aff8c419db97de307d3
Lenar Khusainov <work@lenarx.ru>
2022-07-26 06:34:02 +0300

    Group permissions by module

--- modules/role/components/RolePermissionForm.vue
+++ modules/role/components/RolePermissionForm.vue
@@ -1,13 +1,23 @@
 <template>
     <v-form v-if="form" @submit.prevent="savePermissions">
-        <v-switch
-            v-for="permission in form.permissions"
-            :key="permission.id"
-            v-model="permission.level"
-            inset
-            :label="permission.description"
-            hide-details
-        />
+        <v-expansion-panels>
+            <v-expansion-panel v-for="(group, name) in groupedPermissions" :key="name">
+                <v-expansion-panel-header class="subtitle-1">
+                    {{ name }}
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                    <v-switch
+                        v-for="permission in group"
+                        :key="permission.id"
+                        :input-value="permission.level"
+                        inset
+                        :label="permission.description"
+                        hide-details
+                        @change="updatePermission(permission.id, $event)"
+                    />
+                </v-expansion-panel-content>
+            </v-expansion-panel>
+        </v-expansion-panels>
         <v-row class="expansion-panel-actions mt-5">
             <v-col>
                 <v-btn type="submit" color="green" class="white--text text-uppercase">Сохранить</v-btn>
@@ -20,6 +30,7 @@
 import Form from "form-backend-validation";
 import Role from "~/modules/role/models/Role";
 import Permission from "~/modules/role/models/Permission";
+import groupBy from 'lodash/groupBy';
 
 export default {
     props: {
@@ -40,11 +51,20 @@ export default {
         },
         form: null
     }),
+    computed: {
+        groupedPermissions() {
+            return groupBy(this.form?.permissions ?? [], 'module')
+        }
+    },
     methods: {
+        updatePermission(id, level) {
+            const permission = this.form.permissions.find((permission) => permission.id === id)
+            permission.level = level
+        },
         async loadPermissions() {
             try {
                 const permissions = await Permission.custom('admin/permissions/all').select({
-                    permissions: ['id', 'description']
+                    permissions: ['id', 'description', 'module']
                 }).orderBy('description').$get()
 
                 this.form.permissions = permissions.map((permission) => ({
